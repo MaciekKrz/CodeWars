2297fd46affaf36011589bc135ca778978bc6694
Maciej Krzanowski <krzanowski.maciek@gmail.com>
2018-08-26 18:05:26 +0200

    Function Parsing HTML tags.

---
+++ HTML_Parser.py
@@ -0,0 +1,38 @@
+"""
+Parsing HTML tags
+"""
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start :", tag)
+        for attr in attrs:
+            if attr[1] is None:
+                value = 'None'
+            else:
+                value = attr[1]
+            print("->", attr[0] + " > " + value)
+
+    def handle_endtag(self, tag):
+        print("End   :", tag)
+
+    def handle_startendtag(self, tag, attrs):
+        print("Empty :", tag)
+        for attr in attrs:
+            if attr[1] is None:
+                value = 'None'
+            else:
+                value = attr[1]
+            print("->", attr[0] + " > " + value)
+
+
+html = ""
+
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
+
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
